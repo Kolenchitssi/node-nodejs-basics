ad6781b36ef07e27e61c243c3587b1d039418af2
Kolenchits Sergy <Kolenchitssi@gmail.com>
2022-12-03 15:53:55 +0300

    feat: add solution for cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+  let numberIteration = 1;
+  for (let i = 0; i < args.length; i += 2) {
+    args[i] =
+      numberIteration === 1 ? `propName is` : `prop${numberIteration}Name is`;
+    numberIteration++;
+  }
+  const result = args.join(" ");
+  console.log(result);
 };
 
 parseArgs();
