4bff96457e6339b6587f3f86cf64f4f758856b21
Kolenchits Sergy <Kolenchitssi@gmail.com>
2022-12-03 13:49:30 +0300

    feat: add solution for create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await writeFile("./files/fresh.txt", "I am fresh and young");
+    console.log("Success create file");
+  } catch (error) {
+    console.error("Error create file", error.message);
+  }
 };
 
 await create();
